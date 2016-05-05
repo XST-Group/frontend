cdbdce6f3bae03890092f72cb44ac966e1f82699
KOI <1428757462@qq.com>
2016-05-05 08:48:51 +0800

    方法换成get

--- src/js/global.js
+++ src/js/global.js
@@ -1,6 +1,5 @@
 function showLoginBox(){
-	login_box_template = '<div id="login-modal"><div style="width:100%;height:100%;position:fixed;z-index:2000;top:0;left:0;overflow:hidden" onclick="hideLoginBox()"><div style="height:100%;opacity:.4;background:#000"></div></div><div class="modal"><div class="modal-header clearfix"><h2 class="l">登录</h2><div class="register pull-right">      没有账号？<a href="${rootPath}/register"><strong>立即注册</strong></a></div></div><div class="modal-body"><form id="loginForm"><div class="control-group"><div class="user-id"><input id="username" type="text" placeholder="请输入邮箱/用户名" name="email" onfocus="loginInputFocus(this)" onblur="loginInputBlur(this)" /></div></div><div class="control-group"><div class="user-pw"><input id="username" type="password" name="password" placeholder="请输入密码" onfocus="loginInputFocus(this)" onblur="loginInputBlur(this)" /></div></div><div class="errorMsg" style="color:red;font-size:12px"></div><div class="control-group"><div class="r"><button type="submit" onclick="userLogin()" class="btn btn-primary btn-login">登录</button></div></div></form></div></div></div>'
-	$("body").append(login_box_template);
+	$("#login-modal").css('display','block');
 }
 
 
@@ -13,7 +12,7 @@ function loginInputBlur(input){
 }
 
 function hideLoginBox(){
-	$("#login-modal").remove();
+	$("#login-modal").css('display','none');
 }
 
 
